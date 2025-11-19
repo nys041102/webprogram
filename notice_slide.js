5ee8eb373caaef406381cdc223c3e11076460fe0
nys041102 <nayongsa1102@naver.com>
2025-11-19 22:48:59 +0900

    59

--- notice_slide.js
+++ notice_slide.js
@@ -1,21 +1,15 @@
-// ======= 캐러셀 기능 ========
-
-// 트랙 선택
 const track = document.querySelector(".carousel-track");
-
-// 버튼 선택
 const btnLeft = document.querySelector(".carousel-btn.left");
 const btnRight = document.querySelector(".carousel-btn.right");
 
-// 이동 거리 (카드 한 개 너비)
-const moveAmount = 420;
+const cardWidth = 350; // 카드 + 간격
 
-// 왼쪽 버튼
-btnLeft.addEventListener("click", () => {
-  track.scrollLeft -= moveAmount;
+// 오른쪽 이동
+btnRight.addEventListener("click", () => {
+  track.scrollLeft += cardWidth;
 });
 
-// 오른쪽 버튼
-btnRight.addEventListener("click", () => {
-  track.scrollLeft += moveAmount;
+// 왼쪽 이동
+btnLeft.addEventListener("click", () => {
+  track.scrollLeft -= cardWidth;
 });
