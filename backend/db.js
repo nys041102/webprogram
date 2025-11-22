9824ffb95ebd79493b96b0d011bbd57a29bcc73f
nys041102 <nayongsa1102@naver.com>
2025-11-22 18:37:13 +0900

    82

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "nys041102",
-  database: "test"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 module.exports = pool.promise();
